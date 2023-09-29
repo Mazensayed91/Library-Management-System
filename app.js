653d485416723d4a3efc86ad07613b9c332cfdcc
Mazen Sayed <mazen.sayed@robustastudio.com>
2023-09-29 17:11:06 +0300

    Implement rate limiting for selected API endpoints

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const bookRoutes = require('./routes/books');
 const borrowerRoutes = require('./routes/borrowers');
 const bookcheckoutRoutes = require('./routes/bookcheckout');
 const reportingRoutes = require('./routes/reporting');
+const rateLimit = require('./middlewares/rateLimit');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/api/books', bookRoutes);
-app.use('/api/borrowers', borrowerRoutes);
+
+app.use('/api/books', rateLimit, bookRoutes);
+app.use('/api/borrowers', rateLimit, borrowerRoutes);
 app.use('/api/checkout', bookcheckoutRoutes);
 app.use('/api/report', reportingRoutes);
 
